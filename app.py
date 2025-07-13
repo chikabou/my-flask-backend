ddb1a5f9a96f271473673ea54d94fb6ff473e177
chikabou <123035506+chikabou@users.noreply.github.com>
2025-07-13 01:17:37 +0100

    app.py

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "Flask server is running!"
+
 @app.route('/process', methods=['POST'])
 def process():
     data = request.json
